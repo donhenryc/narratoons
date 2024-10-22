24c31cf39642bad8f91926ec5eaf8e4d4cb5c85a
Fithub10 <Xavi.domingo95@gmail.com>
2024-10-22 21:03:25 +0200

    Actualizacion footer+Formulario

--- src/footer.php
+++ src/footer.php
@@ -1,3 +1,15 @@
+    <footer>
+        <div class="footer">
+            <div>
+                <p>&copy; <?php echo date("Y"); ?> Narratoons.Todos los derechos reservados al alumnado de Inkor Formaciones que desarrollo el proyecto.</p>
+            </div>
+            <div>
+                <p>Dirección: 742 Evergreen Terrace</p>
+                <p>Contacto: +34 000 000 000</p>
+                <p>Soporte tecnico: soporte@narratons.com</p>
+            </div>
+        </div>
+    </footer>
 <script
       src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
       integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
